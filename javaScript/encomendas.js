5da1cb3fc914376f8aed3db66b8675a26ee6d5b6
Quézia Freire <qoliveira809@gmail.com>
2025-08-18 21:09:30 -0300

    Calculo do Total Tabela "Encomenda"s
    calculo onde multiplica a quantidade e o valor unitario do pedido.

--- javaScript/encomendas.js
+++ javaScript/encomendas.js
@@ -1,1 +1,26 @@
-document.querySelector(".total").textContent =document.querySelector(".qtde").textContent * document.querySelector(".unitario").textContent
+//document.querySelector(".total").textContent =document.querySelector(".qtde").textContent * document.querySelector(".unitario").textContent
+
+//Captura as encomendas da tabela
+var clientes = document.querySelectorAll(".cliente");
+
+//Passa por tds encomendas e calcula o valor total
+for (var count = 0; count < clientes.length; count++) {
+
+    //quantidade da encomenda
+    var tab_qtde = clientes[count].querySelector(".qtde").textContent; 
+
+    //valor unitario da encomenda
+    var tab_unitario = clientes[count].querySelector(".unitario").textContent;
+
+    //calcula e exibe o total (chamando a function "calculaTotal")
+    clientes[count].querySelector(".total").textContent = calculaTotal(tab_qtde,tab_unitario);
+}
+
+//função de cálculo do valor total (outra alternativa)
+function calculaTotal(qtde,unitario) {
+ var total=0;
+ 
+ total = qtde * unitario;
+
+ return total;
+}
