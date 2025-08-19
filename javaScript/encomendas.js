0d954a69672898e4084232e3422a54e2c9fc5a6e
Quézia Freire <qoliveira809@gmail.com>
2025-08-18 22:13:35 -0300

    Adicionando formatação para R$

--- javaScript/encomendas.js
+++ javaScript/encomendas.js
@@ -7,20 +7,51 @@ var clientes = document.querySelectorAll(".cliente");
 for (var count = 0; count < clientes.length; count++) {
 
     //quantidade da encomenda
-    var tab_qtde = clientes[count].querySelector(".qtde").textContent; 
+    var tab_qtde = clientes[count].querySelector(".qtde").textContent;
 
     //valor unitario da encomenda
     var tab_unitario = clientes[count].querySelector(".unitario").textContent;
 
-    //calcula e exibe o total (chamando a function "calculaTotal")
-    clientes[count].querySelector(".total").textContent = calculaTotal(tab_qtde,tab_unitario);
+    if (tab_qtde < 1 || isNaN(tab_qtde)) {    //Verifica se a quantidade é válida
+
+        //A quantidade eh menor que 1 ou nao eh numerica
+        clientes[count].querySelector(".qtde").textContent = "Valor inválido!";
+
+        //linha td fica vermelha:
+        //clientes[count].style.backgroundColor = "red";
+
+        //texto da quantidade em vermelho
+        clientes[count].querySelector(".qtde").style.color = "red";
+
+    } else if (tab_unitario < 1 || isNaN(tab_unitario)) {   //validando o valor unit.
+
+        //o valor unitario eh menor que 1 ou nao eh numerico
+        clientes[count].querySelector(".unitario").textContent = "Valor inválido!";
+
+        //texto da quantidade em vermelho
+        clientes[count].querySelector(".unitario").style.color = "red";
+
+    } else {
+        //calcula e exibe o total (chamando a function "calculaTotal")
+        clientes[count].querySelector(".total").textContent = calculaTotal(tab_qtde, tab_unitario);
+
+        //envia formatação para o valor unitario
+        clientes[count].querySelector(".unitario").textContent=formataValor(parseFloat(tab_unitario));
+    }
 }
 
 //função de cálculo do valor total (outra alternativa)
-function calculaTotal(qtde,unitario) {
- var total=0;
- 
- total = qtde * unitario;
+function calculaTotal(qtde, unitario) {
+    var total = 0;
 
- return total;
+    total = qtde * unitario;
+
+    return formataValor(total);
+}
+
+//Função que formata os valores em R$
+function formataValor(valor){
+    var valor_formatado = valor.toLocaleString('pt-BR', {style:'currency',currency:'BRL'});
+
+    return valor_formatado;
 }
